6fb07e6c8911ea13cf1627092892ea968e4a5405
test <toderesa97@gmail.com>
2017-07-27 18:42:46 +0200

    update bought system

--- _site/assets/js/product_layout.js
+++ _site/assets/js/product_layout.js
@@ -2,9 +2,13 @@
 var clothes = [{nos:'new', id:0, description:'Obey T-shirt with Tiger Print0', price:0, price_old: undefined, url:'https://dummyimage.com/77x101/000/fff.png'},
 {nos:'new', id:1, description:'Obey T-shirt with Tiger Print1', price:10, price_old: undefined, url:'https://dummyimage.com/77x101/000/fff.png'},
 {nos:'sale', id:2, description:'Obey T-shirt with Tiger Print2', price:20, price_old: undefined, url:'https://dummyimage.com/77x101/000/fff.png'},
-{nos:'sale', id:3, description:'Obey T-shirt with Tiger Print3', price:30, price_old: 35, url:'https://dummyimage.com/77x101/000/fff.png'}];
+{nos:'sale', id:3, description:'Obey T-shirt with Tiger Print3', price:30, price_old: 38, url:'https://dummyimage.com/77x101/000/fff.png'},
+{nos:'sale', id:4, description:'Obey T-shirt with Tiger Print4', price:40, price_old: undefined, url:'https://dummyimage.com/77x101/000/fff.png'},
+{nos:'sale', id:5, description:'Obey T-shirt with Tiger Print5', price:50, price_old: 35, url:'https://dummyimage.com/77x101/000/fff.png'},
+{nos:'new', id:6, description:'Obey T-shirt with Tiger Print6', price:60, price_old: 45, url:'https://dummyimage.com/77x101/000/fff.png'},
+{nos:'sale', id:7, description:'Obey T-shirt with Tiger Print7', price:70, price_old: undefined, url:'https://dummyimage.com/77x101/000/fff.png'},
+{nos:'new', id:8, description:'Obey T-shirt with Tiger Print8', price:80, price_old: undefined, url:'https://dummyimage.com/77x101/000/fff.png'}];
 var arr = [];
-
 
 $(document).ready(function(){
 	if (sessionStorage.getItem('total') === null) {
@@ -19,6 +23,12 @@ $(document).ready(function(){
 		$('#products-display').html(sessionStorage.getItem('shop-cart-products'));
 	}
 	
+	if (sessionStorage.getItem('buys') === null) {
+		sessionStorage.setItem('buys', 0);
+	} else {
+		$('#the_bag span').html(sessionStorage.getItem('buys'));
+	}
+
 	var products = "";
 	for(var i=0; i<clothes.length; i++) {
 		products += `
@@ -29,7 +39,7 @@ $(document).ready(function(){
 						<img src="assets/img/dummy.jpg" class="img-responsive std-model">
 						<div class="bottom-panel-img">
 							<ul class="no-style">
-								<li><a onclick="hey(${clothes[i].id})" href="#">ADD TO CART</a></li>
+								<li><a onclick="return hey(${clothes[i].id});" href="#">ADD TO CART</a></li>
 								<li class="eye"><a href="#"><i class="ion-eye lenght-md"></i></a></li>
 								<li class="heart"><a href="#"><i class="ion-heart lenght-md"></i></a></li>
 								<li class="stats"><a href="#"><i class="ion-stats-bars lenght-md"></i></a></li>
@@ -68,13 +78,15 @@ $(document).ready(function(){
         
     });
 
-    $('#close').click(function() {
-    	$('#shop-cart').css({"right":"-500px"});
-    	$('#overlayer').css({"display":"none"});
-    });
+    $('#close').click(close);
 
-
+    $('#overlayer').click(close);
 });
+
+function close () {
+	$('#shop-cart').css({"right":"-500px"});
+    $('#overlayer').css({"display":"none"});
+}
 
 function findCloth(id) {
 	for (var i=0; i < clothes.length; i++) {
@@ -87,9 +99,8 @@ function findCloth(id) {
 
 function hey(id) {
 	
-	
 	/* check if a certain div is being displayed in order to avoid display it again */
-	if ( $( `#thumb${id}` ).length > 0) return;
+	if ( $( `#thumb${id}` ).length > 0) return false;
 	var cloth = findCloth(id);
 	arr.push(id);
 	sessionStorage.setItem('arr', arr);
@@ -99,7 +110,7 @@ function hey(id) {
 	} else {
 		sessionStorage.setItem('total', (parseInt(sessionStorage.getItem('total'))+parseInt(cloth.price)));
 	}
-	
+	sessionStorage.setItem('buys', parseInt(sessionStorage.getItem('buys'))+1);
 	var thumbnail = `
 		<div class="thumbnail" id="thumb${id}">
 			<img src="${cloth.url}" alt="dummy image">
@@ -113,6 +124,18 @@ function hey(id) {
 	sessionStorage.setItem('shop-cart-products', sessionStorage.getItem('shop-cart-products')+thumbnail);
 
 	$('#products-display').append(thumbnail);
-
+	$('#the_bag span').html(sessionStorage.getItem('buys'));
 	$('#cart-price').html(`<i class="ion-social-usd"></i> ${sessionStorage.getItem('total')}`);
+	return false; // avoid href executing
+}
+
+function remove_item (id) {
+	sessionStorage.setItem('buys', parseInt(sessionStorage.getItem('buys'))-1);
+	$('#the_bag span').html(sessionStorage.getItem('buys'));
+	$(`#thumb${id}`).remove();
+	sessionStorage.setItem('shop-cart-products', $('#products-display').html());
+	var cloth = findCloth(id);
+
+	sessionStorage.setItem('total', (parseInt(sessionStorage.getItem('total'))-parseInt(cloth.price)));
+	$('#cart-price').html(`<i class="ion-social-usd"></i> ${sessionStorage.getItem('total')}`);		
 }
